8ea440be704455f0599a18495dc690b17eaca477
Wasila1999juma <93697322+Wasila1999juma@users.noreply.github.com>
2021-11-17 05:40:44 +0000

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,12 @@
+public class Program{
+    public static void main(String[]args){
+        double phone= 07724574556;
+        System.out.println("samira");
+        System.out.println("Fuon" + "Zanzibar" + "Mwanakwerekwe" + 222 );
+        System.out.println(phone);
+        System.out.println("SUZA");
+        
+        
+    }
+
+}
